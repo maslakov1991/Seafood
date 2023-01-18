2afb3903ed0fe129b9f5eafe72f9790c017721f8
Maslakov Olskeii <a.maslakov1991@gmail.com>
2023-01-18 17:41:01 +0200

    Product-section-done
    Product-section-done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
 $(function(){
 
+    $('.header-slider').slick({
+        dots: true,
+        vertical: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-up.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-down.svg" alt=""></button>',
+    });
+
+    $('.product__name').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        focusOnSelect: true,
+        asNavFor: '.product__content',
+        vertical: true,
+        prevArrow: '<button type="button" class="product-prev"><img src="images/product-prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="product-next"><img src="images/product-next.svg" alt=""></button>',
+      });
+      $('.product__content').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.product__name',
+        fade: true,
+        arrows: false,
+      });
+
 });
