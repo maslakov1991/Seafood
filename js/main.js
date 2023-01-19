0a93364e1d19375650b5c3275951357f3ece2eb6
Maslakov Olskeii <a.maslakov1991@gmail.com>
2023-01-19 18:17:04 +0200

    header-adaptive-done
    header-adaptive-done

--- js/main.js
+++ js/main.js
@@ -1,27 +1,42 @@
-$(function(){
+$(function () {
 
-    $('.header-slider').slick({
-        dots: true,
-        vertical: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-up.svg" alt=""></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-down.svg" alt=""></button>',
-    });
-
-    $('.product__name').slick({
-        slidesToShow: 6,
-        slidesToScroll: 1,
-        focusOnSelect: true,
-        asNavFor: '.product__content',
-        vertical: true,
-        prevArrow: '<button type="button" class="product-prev"><img src="images/product-prev.svg" alt=""></button>',
-        nextArrow: '<button type="button" class="product-next"><img src="images/product-next.svg" alt=""></button>',
-      });
-      $('.product__content').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        asNavFor: '.product__name',
-        fade: true,
+  $('.header-slider').slick({
+    dots: true,
+    vertical: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-up.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-down.svg" alt=""></button>',
+    responsive: [{
+      breakpoint: 361,
+      settings: {
+        dots: false,
         arrows: false,
-      });
+        autoplay: true,
+        autoplayspeed: 10000
+      }
+    }
+
+    ]
+  });
+
+  $('.product__name').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    focusOnSelect: true,
+    asNavFor: '.product__content',
+    vertical: true,
+    prevArrow: '<button type="button" class="product-prev"><img src="images/product-prev.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="product-next"><img src="images/product-next.svg" alt=""></button>',
+  });
+  $('.product__content').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.product__name',
+    fade: true,
+    arrows: false,
+  });
+
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active');
+  });
 
 });
